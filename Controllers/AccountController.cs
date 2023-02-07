5694cb76d762b2cd160230ed24c3297f88775365
Ân Doan <an.doan@alten.nl>
2023-02-07 20:06:48 +0100

    Added UserProfileViewModel.cs and Account views, with a dynamic navbar. Updated all controllers to allow all or only authorized users

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Authentication;
 using Auth0.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authorization;
+using SchoolManagementApp.MVC.Models;
 
 namespace SchoolManagementApp.MVC.Controllers;
 
@@ -24,7 +25,7 @@ public class AccountController : Controller
     [Authorize]
     public IActionResult Profile()
     {
-        return View(new
+        return View(new UserProfileViewModel()
         {
             Name = User.Identity.Name,
             EmailAddress = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Email)?.Value,
